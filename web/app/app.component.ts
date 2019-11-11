f8a40e6e4880754f61e79201cddccd152c7f3aa8
Daniel Bozik <ext.daniel.bozik@tieto.com>
2019-11-11 16:06:05 +0100

    hide languages on appropriate routes

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, InjectionToken, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { IpcService } from './services/ipc.service';
 import { ipcEvents } from '../shared/ipc-events.enum';
 import { Routes } from '../shared/routes.enum';
+import { LoginService } from './services/login.service';
+import { Subject } from 'rxjs';
+
+export const routingToken: InjectionToken<any> = new InjectionToken<any>('routing token');
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [IpcService]
+    providers: [
+        IpcService, 
+        {
+            provide: routingToken, 
+            useFactory: () => new Subject<string[]>()
+        }
+    ]
 })
 export class AppComponent {
     title = 'web';
@@ -17,10 +27,14 @@ export class AppComponent {
         private readonly router: Router,
         private readonly ngZone: NgZone,
         private readonly ipcService: IpcService,
+        @Inject(routingToken) private readonly routing$: Subject<string[]>,
     ) {
-        router.navigate([Routes.LOGIN]);        
+        router.navigate([Routes.LOGIN]);
+        this.routing$.next([Routes.LOGIN]);
 
-        this.ipcService.ipc.on(ipcEvents.ROUTING, (event: any, url: string[]) => {            
+        this.ipcService.ipc.on(ipcEvents.ROUTING, (event: any, url: string[]) => {
+            this.routing$.next(url);
+            
             this.ngZone.run(() => this.router.navigate(url));
         });
     }
--- web/app/language-selection/language-selection.component.ts
+++ web/app/language-selection/language-selection.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { distinctUntilChanged, startWith, switchMap } from 'rxjs/operators';
 import { Language } from '../../../app/Objects';
 import { LanguageService } from '../services/language.service';
 import { LoginService } from '../services/login.service';
+import { ActivatedRoute } from '@angular/router';
+import { routingToken } from '../app.component';
+import { Subject } from 'rxjs';
+import { Routes } from '../../shared/routes.enum';
 
 @Component({
     selector: 'app-language-selection',
@@ -13,15 +17,25 @@ import { LoginService } from '../services/login.service';
 })
 export class LanguageSelectionComponent implements OnInit {
     public languages: Language[];
+    public route: string[];
 
     public languagesControl: FormControl = new FormControl();
+
+
+    public get hidden(): boolean {
+        const hiddenRoutes: string[] = [Routes.EDIT_TEXT, Routes.LOGIN, Routes.READ_TEXT, Routes.SETTINGS, Routes.WORD];
+
+        return !this.route || this.route.length <= 0 || hiddenRoutes.includes(this.route[0]);
+    }
 
 
     constructor(
         private readonly languageService: LanguageService,
         private readonly loginService: LoginService,
         private readonly changeDetection: ChangeDetectorRef,
+        @Inject(routingToken) private readonly routing$: Subject<string[]>,
     ) {
+        routing$.subscribe((route: string[]) => this.route = route);
     }
 
     ngOnInit() {
